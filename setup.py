fbba360ad1eab361c76644d42bc83a988f61cd35
NicasioTn <peeratach.ten@gmail.com>
2023-10-21 19:42:47 -0400

    update setup

--- setup.py
+++ setup.py
@@ -9,3 +9,11 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'PyMuPDF', 'pyqt6
 
 # move the GizmoBox.desktop file to the desktop
 subprocess.run(['mv', gizmorun, desktop])
+
+#clone the testssl.sh repo
+subprocess.run(['git', 'clone', 'https://github.com/drwetter/testssl.sh.git'])
+
+testssl_dest = '/home/kali/Desktop/Linux-ISAN-Security-Gizmo-Box/data/'
+testssl_souce = '/home/kali/Desktop/Linux-ISAN-Security-Gizmo-Box/testssl.sh'
+# move the testssl.sh file to the data folder
+subprocess.run(['mv', testssl_souce, testssl_dest])
