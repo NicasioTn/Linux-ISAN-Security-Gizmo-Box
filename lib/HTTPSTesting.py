7cc33cd619998b52ea008f13d6e4ec8264b899eb
NicasioTn <peeratach.ten@gmail.com>
2023-10-22 14:26:55 -0400

    fix bug and update ui

--- lib/HTTPSTesting.py
+++ lib/HTTPSTesting.py
@@ -14,6 +14,11 @@ from reportlab.lib.units import cm
 from reportlab.platypus import Table, TableStyle
 from reportlab.lib.utils import ImageReader
 
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+pdfmetrics.registerFont(TTFont('Barlow-Regular', '/usr/share/fonts/Barlow-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('Barlow-Bold', '/usr/share/fonts/Barlow-Bold.ttf')) 
+pdfmetrics.registerFont(TTFont('Barlow-Medium', '/usr/share/fonts/Barlow-Medium.ttf'))
 
 class HTTPSTesting(QDialog):
 
@@ -362,24 +367,22 @@ QLineEdit:focus {
         lucky13 = self.label_resultLuck13Https.text()
         
         # Create a PDF canvas
-        current_time = datetime.now()
         file_name = f"/home/kali/Desktop/Linux-ISAN-Security-Gizmo-Box/data/Reports/HTTPS_Testing_Report.pdf"
         self.btn_file_email_https.setText(file_name.split('/')[-1])
-        target = self.lineEdit_https.text()
 
         c = canvas.Canvas(file_name, pagesize=A4)
-        
+    
         # Define colors
         header_color = colors.HexColor('#A7B6D2')  # Light blue
         title_color = colors.HexColor('#0086D5')   # Dark blue
         text_color = colors.black
-
+        table_color = colors.HexColor('#f1f1f1')
         # First Page
         font_size = 12
 
         # Header section
         header_text = "| ISAN Security Gizmo Box |"
-        c.setFont("Helvetica", font_size)
+        c.setFont("Barlow-Regular", font_size)
         c.setFillColor(header_color)
         c.drawString(72, A4[1] - 36, header_text)
 
@@ -393,68 +396,117 @@ QLineEdit:focus {
         c.drawImage(img, img_x, img_y, width=img_width, height=img_height)
 
         # Main title
-        malware_text = "Hypertext Transfer Protocol Secure Testing Report"
-        c.setFont("Helvetica", 20)
+        https_text = "Hypertext Transfer Protocol Secure Testing Report"
+        c.setFont("Barlow-Medium", 20)
         c.setFillColor(text_color)
-        malware_x = (A4[0] - c.stringWidth(malware_text, "Helvetica", 20)) / 2
-        malware_y = img_y - 20
-        c.drawString(malware_x, malware_y, malware_text)
+        https_x = (A4[0] - c.stringWidth(https_text)) / 2
+        https_y = img_y - 30
+        c.drawString(https_x, https_y, https_text)
 
         # Subtitle and current date
-        gizmo_text = "ISAN Security Gizmo Box"
-        gizmo_x = (A4[0] - c.stringWidth(gizmo_text, "Helvetica", 20)) / 2
-        gizmo_y = malware_y - 50
+        gizmo_text = "                      ISAN Security Gizmo Box"
+        gizmo_x = (A4[0] - c.stringWidth(gizmo_text, "Barlow-Medium", 20)) / 2
+        gizmo_y = https_y - 30
 
         current_datetime = datetime.now().strftime("%d %B %Y %I:%M %p")
-        c.setFont("Helvetica", 15)
-        date_x = (A4[0] - c.stringWidth(current_datetime, "Helvetica", 15)) / 2
-        date_y = gizmo_y - 50
+        c.setFont("Barlow-Medium", 15)
+        date_x = (A4[0] - c.stringWidth(current_datetime,)) / 2
+        date_y = gizmo_y - 30
 
-        c.setFont("Helvetica", 20)
+        #c.setFont("Barlow-Regular", 20)
         c.drawString(gizmo_x, gizmo_y, gizmo_text)
-        c.setFont("Helvetica", 15)
+        #c.setFont("Barlow-Regular", 15)
         c.drawString(date_x, date_y, current_datetime)
 
         # Start a new page (Second Page)
         c.showPage()
+        # Second Page Content
+        # Redraw the header on the second page
+        c.setFont("Barlow-Regular", font_size)
+        c.setFillColor(header_color)
+        c.drawString(72, A4[1] - 36, header_text)
+        
+        # Add the "Scan Summary : https://example.com" at the center of the page.
+        vulner_summary_text = f"Scan Summary : {target}"
+        c.setFont("Barlow-Bold", 16)
+        c.setFillColor(title_color)  # Set the text color to dark blue
+        text_width = c.stringWidth(vulner_summary_text, "Barlow-Regular", 16)
+        text_x = (A4[0] - text_width) / 2
+        text_y = (A4[1] - 100)  # Adjust the y-coordinate as needed to center it vertically.
+
+        c.drawString(text_x, text_y, vulner_summary_text)
+
+        # Add the current date and time below the "Scan Summary" text
+        date_time_text = datetime.now().strftime("%d %B %Y %I:%M %p")
+        c.setFont("Barlow-Bold", 12)
+        date_time_x = (A4[0] - c.stringWidth(date_time_text, "Barlow-Regular", 12)) / 2
+        date_time_y = text_y - 20  # Adjust the vertical position as needed
+
+        c.drawString(date_time_x, date_time_y, date_time_text)
+
+        nmap_info_text = """        testssl.sh is a free command line tool which checks a server's service on any port for the support of TLS/SSL ciphers,"""
+        c.setFont("Barlow-Regular", 9)
+        c.setFillColor(text_color)
+        nmap_info_x = 72
+        nmap_info_y = date_time_y - 30  # Adjust the vertical position as needed
+        c.drawString(nmap_info_x, nmap_info_y, nmap_info_text)
+
+        nmap_info_text = """ protocols as well as some cryptographic flaws. """
+        nmap_info_y -= 12  # ลดระยะห่างในแนวดิดของบรรทัด
+        c.drawString(nmap_info_x, nmap_info_y, nmap_info_text)
 
         # Redraw the header on the second page
-        c.setFont("Helvetica", font_size)
+        c.setFont("Barlow-Regular", font_size)
         c.setFillColor(header_color)
         c.drawString(72, A4[1] - 36, header_text)
 
         # Define a function to create a section with title and table
-        def create_section(title, data, title_color, col_widths, row_heights, y_offset):
+        def create_section(data, title_color, col_widths, row_heights, y_offset):
             title_font_size = 14
             title_x = 72
             title_y = A4[1] - 30 - 2 * cm - y_offset
-            c.setFont("Helvetica", title_font_size)
+            c.setFont("Barlow-Regular", title_font_size)
             c.setFillColor(title_color)
-            c.drawString(title_x, title_y, title)
-            c.line(title_x, title_y - 3, title_x + c.stringWidth(title, "Helvetica", title_font_size), title_y - 3)
+        
 
             table = Table(data, colWidths=col_widths, rowHeights=row_heights)
             table.setStyle(TableStyle([
                 ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+                ('FONTNAME', (0, 0), (-1, -1), 'Barlow-Medium'),
                 ('BOTTOMPADDING', (0, -1), (0, 0), 12),
-                ('GRID', (0, 0), (-1, -1), 1, text_color)
+                ('GRID', (0, 0), (-1, -1), 1, table_color)
             ]))
             table.wrapOn(c, 0, 0)
-            table.drawOn(c, 80, title_y - 170)
+            table.drawOn(c, 80, title_y - 160)
 
+        # Head Table 1
+        test_text = "Testing Summary"
+        c.setFont("Barlow-Medium", 12)
+        c.setFillColor(title_color)
+        test_text_x = 72 
+        test_text_y = A4[1] - 210  # ระบุตำแหน่งแนวราบด้านล่างของหัวข้อ
+
+        c.drawString(test_text_x, test_text_y, test_text)
         # Section 1: Testing Summary
         testing_summary_data = [
             ['Domain name', domain_name],
             ['Strict Transport Security', sts],
-            ['Expiration Status', expiration],
-            ['Intermediate Certificate OSCP', cert_ocsp],
+            ['Certificate Expiration', expiration],
+            ['Certificate OCSP', cert_ocsp],
             ['Signature Algorithm', signature],
             ['Certificate Transparency', transparency],
         ]
         col_widths = [200] * 2
         row_heights = [20] * len(testing_summary_data)
-        create_section("Testing Summary", testing_summary_data, title_color, col_widths, row_heights, 0)
+        create_section(testing_summary_data, title_color, col_widths, row_heights, 120)
 
+        # Head Table 2
+        pro_text = "Testing Protocols"
+        c.setFont("Barlow-Medium", 12)
+        c.setFillColor(title_color)
+        pro_text_x = 72 
+        pro_text_y = A4[1] - 390  # ระบุตำแหน่งแนวราบด้านล่างของหัวข้อ
+        c.drawString(pro_text_x, pro_text_y, pro_text)
         # Section 2: Testing Protocols
         testing_protocols_data = [
             ['SSV v2', sslv2],
@@ -466,8 +518,15 @@ QLineEdit:focus {
         ]
         col_widths = [200] * 2
         row_heights = [20] * len(testing_protocols_data)
-        create_section("Testing Protocols", testing_protocols_data, title_color, col_widths, row_heights, 200)
+        create_section(testing_protocols_data, title_color, col_widths, row_heights, y_offset= 280)
 
+        # Head Table 3
+        vul_text = "Testing Vulnerabilities"
+        c.setFont("Barlow-Medium", 12)
+        c.setFillColor(title_color)
+        vul_text_x = 72 
+        vul_text_y = A4[1] - 550  # ระบุตำแหน่งแนวราบด้านล่างของหัวข้อ
+        c.drawString(vul_text_x, vul_text_y, vul_text)
         # Section 3: Testing Vulnerabilities
         testing_vulnerabilities_data = [
             ['POODLE (SSL v3)', poodle],
@@ -479,7 +538,7 @@ QLineEdit:focus {
         ]
         col_widths = [200] * 2
         row_heights = [20] * len(testing_vulnerabilities_data)
-        create_section("Testing Vulnerabilities", testing_vulnerabilities_data, title_color, col_widths, row_heights, 400)
+        create_section(testing_vulnerabilities_data, title_color, col_widths, row_heights, y_offset= 440)
 
         # Save the PDF
         c.save()
@@ -494,7 +553,17 @@ QLineEdit:focus {
         body = self.textEdit_body_email_https.toPlainText()
         file = "/home/kali/Desktop/Linux-ISAN-Security-Gizmo-Box/data/Reports/HTTPS_Testing_Report.pdf"
         
+        # if send email success then remove file
         SendEmail.sending(SendEmail, to_receiver_email, subject_receiver, body, file)
+        HTTPSTesting.remove_file(self, file)
+    
+    def remove_file(self, file):
+        # remove png
+        for i in range(10):
+            subprocess.run(["rm", f"/home/kali/Desktop/Linux-ISAN-Security-Gizmo-Box/data/ImagesfromPDF/output_page_https_{i}.png"])
+
+        # remove pdf
+        subprocess.run(["rm", file])
     
     def convert_pdf_to_png(self):
         import os
