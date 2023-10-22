6f1e0de8a71c0235b229042248faad2cd54d61a3
NicasioTn <peeratach.ten@gmail.com>
2023-10-21 20:30:32 -0400

    fix bug

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import subprocess
 import sys
-from setuptools import setup, find_packages
 
 gizmorun = '/home/kali/Desktop/Linux-ISAN-Security-Gizmo-Box/GizmoBox.desktop'
 desktop = '/home/kali/Desktop/'
+
 # pip install PyMuPDF pyqt6-tools PyQt6-Qt6 reportlab configparser requests qrcode
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'PyMuPDF', 'pyqt6-tools', 'PyQt6-Qt6', 'reportlab', 'configparser', 'requests', 'qrcode'])
 
@@ -15,5 +15,6 @@ subprocess.run(['git', 'clone', 'https://github.com/drwetter/testssl.sh.git'])
 
 testssl_dest = '/home/kali/Desktop/Linux-ISAN-Security-Gizmo-Box/data/'
 testssl_souce = '/home/kali/Desktop/testssl.sh'
+
 # move the testssl.sh file to the data folder
 subprocess.run(['mv', testssl_souce, testssl_dest])
